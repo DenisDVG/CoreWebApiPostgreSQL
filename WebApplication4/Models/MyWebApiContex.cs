57f3e69e853fc54f7d814adc340205cb090afbbf
DenisGromenkoV <156789qw@gmail.com>
2020-11-08 11:40:21 +0200

    first commit

---
+++ WebApplication4/Models/MyWebApiContex.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApplication4.Models
+{
+    public class MyWebApiContex : DbContext
+    {
+        public MyWebApiContex(DbContextOptions<MyWebApiContex> options) : base(options)
+        {
+        }
+        public DbSet<User> User { get; set;}
+    }
+}
